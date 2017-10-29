b9c20babc6aed30e4d26ee65e5cd6de7c2ebc9e6
Matt Gale <matt@littleball.co.uk>
2017-10-29 20:53:04 +0000

    Updated curry and flip to ignore additional arguments

--- tests/f.flip.test.js
+++ tests/f.flip.test.js
@@ -5,8 +5,9 @@ const {flip} = load('../lib/f.mjs')
 const add = (a, b) => a + b
 
 test('flip', t => {
-  t.is(flip(add)('x')('y'), 'yx')
-  t.is(flip(add, 'a', 'b'), 'ba')
-  t.is(flip(add, 'c')('d'), 'dc')
-  t.is(flip(add)('e', 'f'), 'fe')
+  t.is(flip(add)('x')('y'), 'yx', 'should work passing argument in individually')
+  t.is(flip(add, 'a', 'b'), 'ba', 'should work passing all arguments at once')
+  t.is(flip(add, 'c')('d'), 'dc', 'should work passing some arguments straight away')
+  t.is(flip(add)('e', 'f'), 'fe', 'should work passing in arguments later')
+  t.is(flip(add, 'x', 'y', 'z'), 'yx', 'should ignore additional arguments')
 })
